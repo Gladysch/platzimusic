73a56e6bbc451b82c09177a71c70160b89f6f5f8
Gladys Checasaca <gchecasaca@lexuseditores.com>
2018-05-03 16:02:59 -0500

    Inicializando commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+// Application name 	platzimusic
+// API key 	04c57ea45c7a019b6b9fff5cc3506858
+// Shared secret 	d29ac87d924d297aef764668d468d95b
+// Registered to 	gladysch
+
+export default {
+	appname: 'platzimusic',
+	apiKey: '04c57ea45c7a019b6b9fff5cc3506858',
+	secret: 'd29ac87d924d297aef764668d468d95b',
+	RegisteredTo: 'gladysch'
+}
